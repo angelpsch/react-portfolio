efecc661886f5242c8c161b72d53d3931ca22569
angelpsch <angelpsch@users.noreply.github.com>
2020-08-11 12:25:35 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.9a1e103337f0b2dac56efc1f58628f9e.js"
+  "/react-portfolio/precache-manifest.e93830b7077a45c248e647ebd2f4d1d4.js"
 );
 
 self.addEventListener('message', (event) => {
