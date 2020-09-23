7e37d136e358eb9c702503087dce1e4bdd5b0346
Angel <dh_vkdyv6@ps626549.dreamhostps.com>
2020-09-22 19:54:18 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.010c348fd4a3809f90efca813bcffa66.js"
+  "./precache-manifest.7e1eee91c4eb11eaf52152b77963d274.js"
 );
 
 self.addEventListener('message', (event) => {
